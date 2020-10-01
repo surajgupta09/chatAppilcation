1de7e6df407b7ac815ab568faf86f3ebd0cc15ef
Suraj <53449213+surajgupta09@users.noreply.github.com>
2020-10-01 13:27:48 +0530

    Add files via upload

--- ChatThread1.java
+++ ChatThread1.java
@@ -3,22 +3,13 @@
 
 */
 
-
-
-
-
-
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-class ChatServer1 implements Runnable
-{		
-
-		public void run()
-		{
-
-		try{	
+class ChatServer1 implements Runnable {
+	public void run() {
+		try {
 			ServerSocket server1 = new ServerSocket(6666);	//server socket connection req
 			System.out.println("****************	Waiting for the Connection	****************");
 			Socket client1 = server1.accept();	//server accept the client request
@@ -29,71 +20,46 @@ class ChatServer1 implements Runnable
 
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	//object creation for input
 
-			String msgin="",msgout="";
-			while(!msgin.equals("exit"))		//logic for msg send and recieve
-			{	
+			String msgin="";
+			while (!msgin.equals("exit")) {	
 				msgin=din.readUTF();
 				System.out.println("[Client] : "+msgin);
-				//System.out.print("[Server] : ");
-				//msgout = br.readLine();
-				//dout.writeUTF(msgout);
-				//dout.flush();
 			}
-			
-			client1.close();	//resourse deallocated
-			}
-				catch(Exception e)
-				{
+			client1.close();
+		}catch (Exception e) {
 					System.out.println("****************	session end	****************");
-				}
-		
 		}
 	}
+}
 
-class ChatClient1 implements Runnable
-{		
-	public void run()
-	{	
-		try
-		{		
-			
+class ChatClient1 implements Runnable {		
+	public void run() {	
+		try {		
 			try{Thread.sleep(5000);}catch(Exception e){}	//delay in between server and client requests
 			Socket client1 = new Socket("localhost",7777);	//client send connection request to server
 			System.out.println("Connection established");
+
 			DataInputStream din = new DataInputStream(client1.getInputStream());	//data input object creation
 			DataOutputStream dout = new DataOutputStream(client1.getOutputStream());	//data output object creation
 
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	//object for taking input from user
-			String msgin="",msgout="";
+			String msgout="";
 		
-			while(!msgin.equals("exit"))
-			{	System.out.println("[Server] : ");
+			while (!msgout.equals("exit")) {
 				msgout = br.readLine();
 				dout.writeUTF(msgout);
-				//msgin = din.readUTF();
-				//System.out.println("[Server] : "+msgin);
-			}			
-	
-		}
-			catch(Exception e)
-			{
-				System.out.println("Wrong port number. please try again later");
+				dout.flush();
 			}
+		}catch(Exception e){
+				System.out.println("Wrong port number. Please try again later !!!");
+		}
 	}
  } 
 
- class ChatThread1
- {
- 	public static void main(String[] args) throws Exception
- 	{
- 		Runnable th1 = new ChatServer1();		//object of server class
- 		Runnable th2 = new ChatClient1();		//object of client class
- 		
- 		Thread t1 = new Thread(th1);			//thread allocated to server object
- 		Thread t2 = new Thread(th2);			//thread allocated to client object
- 		
- 		t1.start();		//thread called for force start
- 		t2.start();
- 		
+ class ChatThread1 {
+ 	public static void main(String[] args) throws Exception {
+ 		// starting two different Threads for client ans server uniquely
+ 		new Thread(new ChatServer1()).start();			
+ 		new Thread(new ChatClient1()).start();		
  	}
  }
--- ChatThread2.java
+++ ChatThread2.java
@@ -1,23 +1,13 @@
-/*		
-	
-	
-		
-			************************this code is for machine second****************************
+/*				
+************************this code is for machine second****************************
 */
-
-
-
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-class ChatServer2 implements Runnable
-{		
-
-		public void run()
-		{
-
-		try{	
+class ChatServer2 implements Runnable {
+	public void run() {
+		try {
 			ServerSocket server2 = new ServerSocket(7777);		// server started and waiting for connection
 			System.out.println("****************	Waiting for the Connection	****************");
 			Socket client2 = server2.accept();		// server accept the cliecnt request
@@ -27,56 +17,40 @@ class ChatServer2 implements Runnable
 			DataOutputStream dout = new DataOutputStream(client2.getOutputStream());	//object for output data on server
 
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //object for taking input from user
+			String msgin="";
 
-			String msgin="",msgout="";
-			while(!msgin.equals("exit"))
-			{	
+			while (!msgin.equals("exit")) {	
 				msgin=din.readUTF();
 				System.out.println("[Server] : "+msgin);
-				//System.out.print("[Server] : ");
-				//msgout = br.readLine();
-				//dout.writeUTF(msgout);
-				//dout.flush();
 			}
-			
 			client2.close();
-			}
-				catch(Exception e)
-				{
-					System.out.println("****************	session end	****************");
-				}
-		
+		}catch (Exception e) {
+			System.out.println("****************	session end	****************");
 		}
 	}
+}
 
-class ChatClient2 implements Runnable
-{		
-	public void run()
-	{	
-		try
-		{
+class ChatClient2 implements Runnable {		
+	public void run() {	
+		try {
 			//try{Thread.sleep(5000);}catch(Exception e){}			//delay for request of client and server
 			Socket client2 = new Socket("localhost",6666);		//client request to particular server
 			System.out.println("Connection established");
+			
 			DataInputStream din = new DataInputStream(client2.getInputStream());
 			DataOutputStream dout = new DataOutputStream(client2.getOutputStream());
 
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //object for taking input from user
-			String msgin="",msgout="";
+			String msgout="";
 		
-			while(!msgin.equals("exit"))
-			{	System.out.println("[Client] : ");
+			while (!msgout.equals("exit")) {
 				msgout = br.readLine();
 				dout.writeUTF(msgout);
-				//msgin = din.readUTF();
-				//System.out.println("[Server] : "+msgin);
-			}			
-	
-		}
-			catch(Exception e)
-			{
-				System.out.println("Wrong port number. please try again later");
+				dout.flush();
 			}
+		}catch (Exception e) {
+			System.out.println("Wrong port number. please try again later");
+		}
 	}
  } 
  
@@ -84,14 +58,8 @@ class ChatClient2 implements Runnable
  {
  	public static void main(String[] args) throws Exception
  	{	
- 		Runnable th1 = new ChatServer2();	//object of server class
- 		Runnable th2 = new ChatClient2();	//object of client class
-
- 		Thread t1 = new Thread(th1);		//thread allocated to server
- 		Thread t2 = new Thread(th2);		//thread alllocated to client
- 		
- 		t1.start();
- 		t2.start();
+ 		new Thread(new ChatServer2()).start();			
+ 		new Thread(new ChatClient2()).start();	
  		
  		
  	}
